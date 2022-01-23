3ea48f55add2e05d1c6b4ef7f76b075866ecd328
Dan <dan@techsaviours.org>
2022-01-23 17:14:39 +1300

    added start.sh script

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]
+then
+echo "This script must be run as root"
+exit 1
+fi
+
+read -r -p "Do you want to install a server repository or do you want to add a client? [SERVER/client] " prompt
+
+if [[ $prompt == "SERVER" ]]
+then
+./scripts/server.sh
+
+elif [[ $prompt == "client" ]]
+then
+./scripts/client.sh
+
+else
+echo "Nothing has been selected."
+fi
