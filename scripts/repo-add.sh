4f29ee340fccdf1c5820185a690301e00a3a395f
Dan <dan@techsaviours.org>
2022-02-21 16:25:46 +1300

    added ARMv7 architecture

--- scripts/repo-add.sh
+++ scripts/repo-add.sh
@@ -9,6 +9,14 @@ repo-add --new /srv/http/repo/archlinux/aarch64/aur/homerepo-aur.db.tar.zst /srv
 repo-add --new /srv/http/repo/archlinux/aarch64/aur/homerepo-aur.db.tar.zst /srv/http/repo/archlinux/aarch64/aur/*pkg.tar.zst
 repo-add --new /srv/http/repo/archlinux/aarch64/aur/homerepo-aur.db.tar.zst /srv/http/repo/archlinux/aarch64/aur/*pkg.tar.gz
 
+# armv7l
+repo-add --new /srv/http/repo/archlinux/armv7l/homerepo.db.tar.zst /srv/http/repo/archlinux/armv7l/*pkg.tar.xz
+repo-add --new /srv/http/repo/archlinux/armv7l/homerepo.db.tar.zst /srv/http/repo/archlinux/armv7l/*pkg.tar.zst
+repo-add --new /srv/http/repo/archlinux/armv7l/homerepo.db.tar.zst /srv/http/repo/archlinux/armv7l/*pkg.tar.gz
+repo-add --new /srv/http/repo/archlinux/armv7l/aur/homerepo-aur.db.tar.zst /srv/http/repo/archlinux/armv7l/aur/*pkg.tar.xz
+repo-add --new /srv/http/repo/archlinux/armv7l/aur/homerepo-aur.db.tar.zst /srv/http/repo/archlinux/armv7l/aur/*pkg.tar.zst
+repo-add --new /srv/http/repo/archlinux/armv7l/aur/homerepo-aur.db.tar.zst /srv/http/repo/archlinux/armv7l/aur/*pkg.tar.gz
+
 # x86_64
 repo-add --new /srv/http/repo/archlinux/x86_64/homerepo.db.tar.zst /srv/http/repo/archlinux/x86_64/*pkg.tar.xz
 repo-add --new /srv/http/repo/archlinux/x86_64/homerepo.db.tar.zst /srv/http/repo/archlinux/x86_64/*pkg.tar.zst
